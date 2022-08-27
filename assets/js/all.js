35ab8103a8f4d0417cb41fecc65c8387cdc23e01
Ryan Chiang <wsad71155@gmail.com>
2022-08-27 18:19:05 +0800

    Update 2022-08-27T10:19:03.774Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,7 +3,7 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
-var swiper = new Swiper(".lessonTag-swiper", {
+var lessonTagSwiper = new Swiper(".lessonTag-swiper", {
   // Default parameters
   slidesPerView: 3,
   spaceBetween: 8,
@@ -28,9 +28,8 @@ var swiper = new Swiper(".lessonTag-swiper", {
   autoplay: {
     delay: 2000
   }
-}); // Swiper
-
-var Swiper = new Swiper('.swiper', {
+});
+var reviewSwiper = new Swiper('.review-swiper', {
   // Optional parameters
   effect: 'fade',
   fadeEffect: {
@@ -38,12 +37,38 @@ var Swiper = new Swiper('.swiper', {
   },
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination'
+    el: '.review-swiper-pagination'
   },
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
+    nextEl: '.review-swiper-button-next',
+    prevEl: '.review-swiper-button-prev'
+  }
+});
+var lessonTypeSwiper = new Swiper(".lessonType-swiper", {
+  // Default parameters
+  slidesPerView: 2,
+  spaceBetween: 16,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 16
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 16
+    },
+    // when window width is >= 640px
+    992: {
+      slidesPerView: 6,
+      spaceBetween: 16
+    }
+  },
+  autoplay: {
+    delay: 2000
   }
 });
 //# sourceMappingURL=all.js.map
