2675bd9ea63d9c9900b1ffcbe612e90eaf357b44
Ryan Chiang <wsad71155@gmail.com>
2022-08-27 15:18:35 +0800

    'week6'

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -30,3 +30,23 @@ var swiper = new Swiper(".lessonTag-swiper", {
     delay: 2000,
   },
 });
+
+// Swiper
+const Swiper = new Swiper('.swiper', {
+  // Optional parameters
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
