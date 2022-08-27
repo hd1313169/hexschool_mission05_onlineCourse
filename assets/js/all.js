928403311fd118a19c24461619fa524479905b2d
Ryan Chiang <wsad71155@gmail.com>
2022-08-27 15:18:16 +0800

    Update 2022-08-27T07:18:13.508Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -28,5 +28,22 @@ var swiper = new Swiper(".lessonTag-swiper", {
   autoplay: {
     delay: 2000
   }
+}); // Swiper
+
+var Swiper = new Swiper('.swiper', {
+  // Optional parameters
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
 });
 //# sourceMappingURL=all.js.map
